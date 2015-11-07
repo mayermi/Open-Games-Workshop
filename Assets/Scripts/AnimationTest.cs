5d3fd89cbc439e09e3d9f5218434763094c7c8c3
unknown <matthiasnefzger.mail@gmail.com>
2015-11-07 13:38:57 +0100

    erster Test mit Animationen aus Blender

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour {
+	
+	Animation animation;
+	Camera camera;
+
+	void Start() {
+		animation = GetComponent<Animation> ();
+		camera = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			animation.Play("Run");
+		}
+
+		if (animation.IsPlaying ("Run")) {
+			transform.position += 0.1f * Vector3.forward;
+		}
+
+	}
+}
