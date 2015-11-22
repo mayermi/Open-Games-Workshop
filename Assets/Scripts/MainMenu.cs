69a792244ba19bb748d97e7c260c87bd03191816
unknown <matthiasnefzger.mail@gmail.com>
2015-11-22 15:40:08 +0100

    Main Menu Test Setup

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    GameObject planet;
+
+    void Start()
+    {
+        planet = GameObject.Find("Planet");
+    }
+
+    void Update()
+    {
+        planet.transform.RotateAround(planet.transform.forward, Time.deltaTime/-2f);
+    }
+
+    public void LoadScene()
+    {
+        Application.LoadLevel("sphere_movement");
+    }
+}
