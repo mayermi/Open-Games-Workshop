6f14b47d466b0c9dfd9316385066e30e7259ad46
Alllexa <a-ronge@gmx.de>
2015-12-20 18:38:25 +0100

    camerarotation without restriction and also without orientation loss

--- Assets/Scripts/MonoBehaviours/CameraRotation.cs
+++ Assets/Scripts/MonoBehaviours/CameraRotation.cs
@@ -6,41 +6,52 @@ public class CameraRotation : MonoBehaviour {
     public int camDistance = 185;
     GameObject planet;
     Camera cam;
-    float rotationYAxis = 0.0f;
-    float rotationXAxis = 0.0f;
+    float rotationXAxis_local = 0.0f;
+    float rotationYAxis_local = 0.0f;
     float fov;
     public float fovSpeed = 8f;
     public float camSpeed = 5f;
+    public Vector3 planetpos;
 
     void Start () {
         planet = GameObject.Find("Planet");
+        planetpos = planet.transform.position;
+        transform.LookAt(planetpos);
         cam = Camera.main;
         fov = cam.fieldOfView;
-        Vector3 angles = cam.transform.eulerAngles;
-        rotationXAxis = angles.x;
-        rotationYAxis = angles.y;
+        Vector3 localAngles = cam.transform.localEulerAngles;
+        rotationXAxis_local = localAngles.x;
+        rotationYAxis_local = localAngles.y;
+
     }
 
     void LateUpdate()
     {
 
-        Vector3 camAngles = cam.transform.eulerAngles;
-
         if (Input.GetKey(KeyCode.A))
         {
-            rotationYAxis += camSpeed * Time.deltaTime;
+            //rotationYAxis_local += camSpeed * Time.deltaTime;
+            Vector3 verticalaxis = transform.TransformDirection(Vector3.down);
+            transform.RotateAround(planet.transform.position, verticalaxis, -camSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rotationYAxis -= camSpeed * Time.deltaTime;
+            //rotationYAxis_local -= camSpeed * Time.deltaTime;
+            Vector3 verticalaxis = transform.TransformDirection(Vector3.up);
+            transform.RotateAround(planet.transform.position, verticalaxis, -camSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W) /*&& (camAngles.x < 30 || camAngles.x > 320)*/)
+        if (Input.GetKey(KeyCode.W))
         {
-            rotationXAxis += camSpeed * Time.deltaTime;
+            //rotationXAxis_local += camSpeed * Time.deltaTime;
+            Vector3 horizontalaxis = transform.TransformDirection(Vector3.left);
+            transform.RotateAround(planet.transform.position, horizontalaxis, -camSpeed * Time.deltaTime);
+
         }
-        if (Input.GetKey(KeyCode.S) /*&& (camAngles.x < 40 || camAngles.x > 330)*/)
+        if (Input.GetKey(KeyCode.S))
         {
-            rotationXAxis -= camSpeed * Time.deltaTime;
+            //rotationXAxis_local -= camSpeed * Time.deltaTime;
+            Vector3 horizontalaxis = transform.TransformDirection(Vector3.right);
+            transform.RotateAround(planet.transform.position, horizontalaxis, -camSpeed * Time.deltaTime);
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
@@ -55,12 +66,13 @@ public class CameraRotation : MonoBehaviour {
 
         cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fov, Time.deltaTime * fovSpeed);       
 
-        Quaternion cameraRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
-        cam.transform.rotation = cameraRotation;
+        /*Quaternion cameraRotation = Quaternion.Euler(rotationXAxis_local, rotationYAxis_local, 0);
+        cam.transform.localRotation = cameraRotation;
 
         // let the camera circle around the planet in a distance of 185
         Vector3 cameraPosition = cameraRotation * new Vector3(0, 0, -camDistance) + planet.transform.position;
-        cam.transform.position = cameraPosition;
+        cam.transform.position = cameraPosition;*/
+
     }
 
 
