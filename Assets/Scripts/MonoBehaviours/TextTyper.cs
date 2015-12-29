93f29ab366c7a6a96f7ddc7970924a46a7650d37
Alllexa <a-ronge@gmx.de>
2015-12-29 17:43:29 +0100

    intro improvements

---
+++ Assets/Scripts/MonoBehaviours/TextTyper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextTyper : MonoBehaviour {
+	
+	private float letterPause = 0.02f;
+	string message;
+	Text textComp;
+	
+	// Use this for initialization
+	void Start () {
+		textComp = GetComponent<Text>();
+		message = textComp.text;
+		textComp.text = "";
+		StartCoroutine(TypeText ());
+	}
+	
+	IEnumerator TypeText () {
+		foreach (char letter in message.ToCharArray()) {
+			textComp.text += letter;
+			yield return 0;
+			yield return new WaitForSeconds (letterPause);
+		}
+	}
+}
