ff2eb24d2b5605b54833170fc629bc1b2707a5a1
unknown <matthiasnefzger.mail@gmail.com>
2016-01-06 15:44:07 +0100

    pushed ImageEffects for Blur Effect for Tutorials

--- Assets/Scripts/MonoBehaviours/TutorialController.cs
+++ Assets/Scripts/MonoBehaviours/TutorialController.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
 
 public class TutorialController : MonoBehaviour {
 
     public GameObject navigation;
     public GameObject skills;
     public GameObject monsters;
+    GameObject cam;
+
 
 	void Start () {
-	    
-	}
+        cam = Camera.main.gameObject;
+        cam.GetComponent<BlurOptimized>().enabled = false;
+    }
 
     void Update()
     {
@@ -21,12 +25,14 @@ public class TutorialController : MonoBehaviour {
 
     public void ShowNavigation()
     {
+        ActivateBlur();
         navigation.SetActive(true);
         Time.timeScale = 0;
     }
 
     public void ShowSkills()
     {
+        ActivateBlur();
         skills.SetActive(true);
         Time.timeScale = 0;
     }
@@ -42,12 +48,19 @@ public class TutorialController : MonoBehaviour {
         navigation.SetActive(false);
         skills.SetActive(false);
         monsters.SetActive(false);
+        cam.GetComponent<BlurOptimized>().enabled = false;
         Time.timeScale = 1f;
+    }
+
+    void ActivateBlur()
+    {
+        cam.GetComponent<BlurOptimized>().enabled = true;
     }
 
     IEnumerator WaitForCamera()
     {
-        yield return new WaitForSeconds(1.25f);
+        yield return new WaitForSeconds(1.1f);
+        ActivateBlur();
         monsters.SetActive(true);
         Time.timeScale = 0;
     }
